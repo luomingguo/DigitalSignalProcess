8f5dea50562f281e3bff740df6255f41e94dfb35
luomingguo <46103355+luomingguo@users.noreply.github.com>
2019-07-02 01:51:11 +0800

    Delete q.m

--- 11/q.m
+++
@@ -1,7 +0,0 @@
-function [y]=Q(x)
-% [y]=Q(x)
-%		Q evaluates the Q-function. 
-%   		y = 1/sqrt(2*pi) * integral from x to inf of exp(-t^2/2) dt.
-%     		y = (1/2) * erfc(x/sqrt(2)).
-y=(1/2)*erfc(x/sqrt(2));
-
